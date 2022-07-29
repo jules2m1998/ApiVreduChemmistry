2db311fd6436df2dce4959c27731a38dacd601d6
jules2m1998 <mevaajules9@gmail.com>
2022-07-29 17:22:54 +0100

    refactor:

--- Dto/Elements/RegisterMoleculeDto.cs
+++ Dto/Elements/RegisterMoleculeDto.cs
@@ -7,9 +7,6 @@ public class RegisterMoleculeDto
     [Required(ErrorMessage = "Nom obligatoire !")]
     public string Name { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Couleur obligatoire !")]
-    public string Color { get; set; } = string.Empty;
-
     public IFormFile? Image { get; set; }
 
     [Required(ErrorMessage = "Texture obligatoire !")]
